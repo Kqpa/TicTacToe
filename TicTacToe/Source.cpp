767c7e6d4705e1edcc19a2333f2ef83c9e05044a
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-02-04 22:51:49 +0300

    Added a few stuff

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -2,99 +2,113 @@
 
 class ticTacToe {
 
-public:
+	public:
 
-	int playerPoint = 0;
-	string playerName;
-	char playerSymbol;
+		int playerPoint = 0;
+		string playerName;
+		char playerSymbol;
 
-	void getChart(char place) {
+		void getChart(char place) {
 
-		for (int i = 0; i < 9; i++) {
+			for (int i = 0; i < 9; i++) {
 
-			if (places[i] == (int)place)
-				places[i] = this->playerSymbol;
+				if (places[i] == (int)place)
+					places[i] = this->playerSymbol;
+
+			}
+
+			system("cls");
+		
+			cout << "\n\n      Tic-Tac-Toe\n\n";
+			cout << "        |     |     " << endl;
+			cout << "     " << GREEN << places[0] << RESET << "  |" << "  " << GREEN << places[1] << RESET << "  |" << "  " << GREEN << places[2] << RESET << endl;
+			cout << "   _____|_____|_____" << endl;
+			cout << "        |     |     " << endl;
+			cout << "     " << GREEN << places[3] << RESET << "  |" << "  " << GREEN << places[4] << RESET << "  |" << "  " << GREEN << places[5] << RESET << endl;
+			cout << "   _____|_____|_____" << endl;
+			cout << "        |     |     " << endl;
+			cout << "     " << GREEN << places[6] << RESET << "  |" << "  " << GREEN << places[7] << RESET << "  |" << "  " << GREEN << places[8] << RESET << endl;
+			cout << "        |     |     " << endl;
 
 		}
-
-		system("cls");
-		
-		cout << "\n\n      Tic-Tac-Toe\n\n";
-		cout << "        |     |     " << endl;
-		cout << "     " << GREEN << places[0] << RESET << "  |" << "  " << GREEN << places[1] << RESET << "  |" << "  " << GREEN << places[2] << RESET << endl;
-		cout << "   _____|_____|_____" << endl;
-		cout << "        |     |     " << endl;
-		cout << "     " << GREEN << places[3] << RESET << "  |" << "  " << GREEN << places[4] << RESET << "  |" << "  " << GREEN << places[5] << RESET << endl;
-		cout << "   _____|_____|_____" << endl;
-		cout << "        |     |     " << endl;
-		cout << "     " << GREEN << places[6] << RESET << "  |" << "  " << GREEN << places[7] << RESET << "  |" << "  " << GREEN << places[8] << RESET << endl;
-		cout << "        |     |     " << endl;
-
-
-	}
 
 };
 
 int main() {
 
-
-	int option = 0;
+	char option = '0';
+	char playerChoice;
+	
 	bool isWon = false;
 	bool isOver = false;
 
 	ticTacToe playerOne, playerTwo;
-	char playerChoice;
 
 	do {
 
-		cout << "Player 1, enter your name: ";
+		cout << "\n\n      Tic-Tac-Toe\n\n";
+
+		cout << "> " << GREEN << "Player 1" << RESET << ", enter your name: ";
 		cin >> playerOne.playerName;
 
 		playerOne.playerSymbol = 'X';
 
-		cout << "Player 2, enter your name: ";
+		cout << "> " << GREEN << "Player 2" << RESET << ", enter your name: ";
 		cin >> playerTwo.playerName;
 
 		playerTwo.playerSymbol = 'O';
 
 		if (playerOne.playerName == playerTwo.playerName)
-			cout << "Player names have to be different." << endl;
+			cout << "> Player names have to be different." << endl;
 
 	} while (playerOne.playerName == playerTwo.playerName);
 
-
-
-
 	while (true) {
 
-		if ((option == 0) || (option == 1)) {
+		if ((option == '0') || (option == 'y')) {
 
 			playerOne.getChart(NULL);
-			option = 100; // Sets to random number so doesnt it doesnt meet the condition
+			option = '-1'; // Sets to random number so doesnt it doesn't meet the condition
 
 		}
 
 		isOver = isGameOver();
 
 		if (isOver == true || isWon == true) {
+			
 			isWon = false;
 			isOver = false;
-			cout << "Game over, play again? (1/2): ";
+			
+			cout << "\n> Game over, play again? (" << GREEN << "y" << RESET << "/" << RED << "n" << RESET << "): ";
 			cin >> option;
+			option = tolower(option);
 
 		}
 
-		if (option == 2) {
-			cout << "P1: " << playerOne.playerPoint << endl;
-			cout << "P2: " << playerTwo.playerPoint << endl;
+		if (option == 'n') {
+
+			int diff = 5;
+
+			if (playerOne.playerName.length() > playerTwo.playerName.length())
+				diff += playerOne.playerName.length();
+			else
+				diff += playerTwo.playerName.length();
+			
+			cout << "\n\n  Player Points:\n";
+			cout << "> " << GREEN << playerOne.playerName << RESET << ": " << std::right << setw(diff - playerOne.playerName.length()) << playerOne.playerPoint << endl;
+			cout << "> " << GREEN << playerTwo.playerName << RESET << ": " << std::right << setw(diff - playerTwo.playerName.length()) << playerTwo.playerPoint << endl;
+
 			break;
+
 		}
-		else if (option == 1) {
+		else if (option == 'y') {
+
 			resetTable();
 			continue;
+
 		}
 
-		cout << playerOne.playerName << ", select a place (1-9): ";
+		cout << endl << "> " << GREEN << playerOne.playerName << RESET << ", pick a square (1-9): ";
 		cin >> playerChoice;
 
 		playerOne.getChart(playerChoice);
@@ -102,35 +116,48 @@ int main() {
 		isWon = checkWin();
 
 		if (isWon == true) {
-			cout << playerOne.playerName << " won!" << endl;
+			
+			cout << endl << "> " << GREEN << playerOne.playerName << RESET << " won!" << endl;
 			playerOne.playerPoint++;
-
-
-			// ask if want to play again
-			// break;
+		
 		}
 
 		isOver = isGameOver();
 
 		if (isOver == true || isWon == true) {
+			
 			isWon = false;
 			isOver = false;
-			cout << "Game over, play again? (1/2): ";
+			
+			cout << "\n> Game over, play again? (" << GREEN << "y" << RESET << "/" << RED << "n" << RESET << "): ";
 			cin >> option;
+			option = tolower(option);
 
 		}
 
-		if (option == 2) {
-			cout << "P1: " << playerOne.playerPoint << endl;
-			cout << "P2: " << playerTwo.playerPoint << endl;
-			break;
-		}
-		else if (option == 1) {
+		if (option == 'n') {
+			
+			int diff = 5;
+
+			if (playerOne.playerName.length() > playerTwo.playerName.length())
+				diff += playerOne.playerName.length();
+			else
+				diff += playerTwo.playerName.length();
+
+			cout << "\n\n  Player Points:\n";
+			cout << "> " << GREEN << playerOne.playerName << RESET << ": " << std::right << setw(diff - playerOne.playerName.length()) << playerOne.playerPoint << endl;
+			cout << "> " << GREEN << playerTwo.playerName << RESET << ": " << std::right << setw(diff - playerTwo.playerName.length()) << playerTwo.playerPoint << endl;
+
+			break; 
+
+		} else if (option == 'y') {
+			
 			resetTable();
 			continue;
+		
 		}
 
-		cout << playerTwo.playerName << ", select a place (1-9): ";
+		cout << endl << "> " << GREEN << playerTwo.playerName << RESET << ", pick a square (1-9): ";
 		cin >> playerChoice;
 
 		playerTwo.getChart(playerChoice);
@@ -138,11 +165,14 @@ int main() {
 		isWon = checkWin();
 
 		if (isWon == true) {
-			cout << playerTwo.playerName << " won!" << endl;
+			
+			cout << endl << "> " << GREEN << playerOne.playerName << RESET << " won!" << endl;
 			playerTwo.playerPoint++;
-			// break;
+
 		}
+
 	}
+
 }
 
 bool checkWin() {
@@ -162,7 +192,6 @@ bool checkWin() {
 		(places[2] == places[4]) && (places[4] == places[6])
 
 		)
-			
 			isWon = true;
 			
 	return isWon;
@@ -188,7 +217,6 @@ bool isGameOver() {
 }
 
 void resetTable() {
-
 
 	places[0] = '1';
 	places[1] = '2';
