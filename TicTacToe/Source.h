23c7c96050ff4f723c7e0f29de1d4702202106c3
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-02-06 20:45:09 +0300

    Update header

--- TicTacToe/Source.h
+++ TicTacToe/Source.h
@@ -24,11 +24,9 @@ using std::setw;
 
 #define RESET   "\033[0m"		/* Reset Color */
 
-#define RED     "\033[31m"      /* Red */
+#define RED     "\033[31m"		/* Red */
 
-#define GREEN   "\033[32m"      /* Green */
-
-#define BLUE	"\033[34m"		/* Blue */
+#define GREEN   "\033[32m"		/* Green */
 
 char places[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
