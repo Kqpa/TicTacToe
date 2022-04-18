45b02bbf68f88db7943808f16e77367a0f3e4e16
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-04-18 19:24:04 +0300

    align

--- src/CheckWin.cpp
+++ src/CheckWin.cpp
@@ -7,14 +7,14 @@ bool CheckWin() {
 
 	bool IsWon = false;
 
-	if 	(((Places[0] == Places[1]) && (Places[1] == Places[2]))	||
-		((Places[3] == Places[4]) && (Places[4] == Places[5]))	||
-		((Places[6] == Places[7]) && (Places[7] == Places[8]))	||
-		((Places[0] == Places[3]) && (Places[3] == Places[6]))	||
-		((Places[1] == Places[4]) && (Places[4] == Places[7]))	||
-		((Places[2] == Places[5]) && (Places[5] == Places[8]))	||
-		((Places[0] == Places[4]) && (Places[4] == Places[8]))	||
-		((Places[2] == Places[4]) && (Places[4] == Places[6]))) {
+	if  (((Places[0] == Places[1]) && (Places[1] == Places[2])) ||
+	     ((Places[3] == Places[4]) && (Places[4] == Places[5])) ||
+	     ((Places[6] == Places[7]) && (Places[7] == Places[8])) ||
+	     ((Places[0] == Places[3]) && (Places[3] == Places[6])) ||
+	     ((Places[1] == Places[4]) && (Places[4] == Places[7])) ||
+	     ((Places[2] == Places[5]) && (Places[5] == Places[8])) ||
+	     ((Places[0] == Places[4]) && (Places[4] == Places[8])) ||
+	     ((Places[2] == Places[4]) && (Places[4] == Places[6]))) {
 			IsWon = true;
 	}
 			
